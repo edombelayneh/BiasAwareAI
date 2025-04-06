7fc10f97d7ff723b41e9a25fd8bd0c1a159d9354
Edom Belayneh <101343364+edombelayneh@users.noreply.github.com>
2025-04-06 11:43:25 -0400

    added light and dark mode

--- app/components/NewPageIcon.js
+++ app/components/NewPageIcon.js
@@ -19,26 +19,6 @@ const NewPageIcon = ({ page, text, imageName }) => {
     };
   
     return (
-      // <Box
-      //   onClick={handleClick}
-      //   sx={{
-      //     cursor: 'pointer',
-      //     display: 'flex',
-      //     flexDirection: 'column',
-      //     alignItems: 'center',
-      //     textAlign: 'center',
-      //     width: isMobile ? 100 : 200,
-      //     padding: isMobile ? 2 : 5,
-      //     borderRadius: '50%',
-      //     backgroundColor: '#FFFFFF',
-      //     boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
-      //     transition: 'transform 0.3s, box-shadow 0.3s',
-      //     '&:hover': {
-      //       transform: 'scale(1.05)',
-      //       boxShadow: '0 6px 20px rgba(156, 39, 176, 0.4)',
-      //     },
-      //   }}
-      // >
       <Box
       onClick={handleClick}
       sx={{
@@ -50,8 +30,7 @@ const NewPageIcon = ({ page, text, imageName }) => {
           transform: 'scale(1.08)',
           boxShadow: '0 0 25px 8px rgba(156, 39, 176, 0.7)',
         },
-      }}
-    >
+      }}>
         <Image
           src={imageName}
           alt={`${text} icon`}
--- app/theme.js
+++ app/theme.js
@@ -1,20 +1,61 @@
+// // app/theme.js
+// import { createTheme } from '@mui/material/styles';
+
+// const theme = createTheme({
+//   palette: {
+//     mode: 'light',
+//     primary: { main: '#9C27B0' },
+//     secondary: { main: '#E6E6FA' },
+//     background: {
+//       default: '#F5F5FA',
+//       paper: '#FFFFFF',
+//     },
+//     text: { primary: '#333' },
+//   },
+//   typography: {
+//     fontFamily: `'Playfair Display', serif`,
+//   },
+// });
+
+// export default theme;
+
+// app/theme.js
 // app/theme.js
 import { createTheme } from '@mui/material/styles';
 
-const theme = createTheme({
+export const lightTheme = createTheme({
   palette: {
     mode: 'light',
     primary: { main: '#9C27B0' },
     secondary: { main: '#E6E6FA' },
     background: {
-      default: '#F5F5FA',
-      paper: '#FFFFFF',
+      default: '#EDEDF3',
+      paper: '#F5F5FA',
     },
-    text: { primary: '#333' },
+    text: {
+      primary: '#2A2A2A',
+    },
   },
   typography: {
     fontFamily: `'Playfair Display', serif`,
   },
 });
 
-export default theme;
+export const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: { main: '#BA68C8' },
+    secondary: { main: '#D1C4E9' },
+    background: {
+      default: '#121212',
+      paper: '#1E1E2F',
+    },
+    text: {
+      primary: '#EDEDED',
+    },
+  },
+  typography: {
+    fontFamily: `'Playfair Display', serif`,
+  },
+});
+
